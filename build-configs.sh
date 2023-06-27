3e4d7add49e115da724dd2bf929fc0aafc6beb10
Michał Bronowski <michal.bronowski@hxs.pl>
2023-06-27 10:16:50 +0200

    node version modifier

--- build-configs.sh
+++ build-configs.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2"
+PHP_VERSIONS="7.3 7.4 8.0 8.2"
+NODE_VERSIONS="14 16 18 19"
 
 for PHP_VERSION in $PHP_VERSIONS;
 do
-    echo "Building php$PHP_VERSION config"
+    for NODE_VERSION in $NODE_VERSIONS;
+    do
 
-    DIR_NAME="php$PHP_VERSION"
-    mkdir -p "$DIR_NAME"
+        echo "Building php$PHP_VERSION node$NODE_VERSION config"
 
-    cp entrypoint.sh "$DIR_NAME/entrypoint.sh"
-    chmod +x "$DIR_NAME/entrypoint.sh"
-    cp sudoers "$DIR_NAME/sudoers"
+        DIR_NAME="php$PHP_VERSION-node$NODE_VERSION"
+        mkdir -p "$DIR_NAME"
 
-    PHP_PACKAGES=$(cat "packages-php$PHP_VERSION.txt" | xargs)
+        cp entrypoint.sh "$DIR_NAME/entrypoint.sh"
+        chmod +x "$DIR_NAME/entrypoint.sh"
+        cp sudoers "$DIR_NAME/sudoers"
 
-    sed "s/\#\#php-packages\#\#/$PHP_PACKAGES/" Dockerfile.template | sed "s/\#\#php-version\#\#/$PHP_VERSION/" > "$DIR_NAME/Dockerfile"
+        PHP_PACKAGES=$(cat "packages-php$PHP_VERSION.txt" | xargs)
+
+        sed "s/\#\#php-packages\#\#/$PHP_PACKAGES/" Dockerfile.template | sed "s/\#\#php-version\#\#/$PHP_VERSION/" > "$DIR_NAME/Dockerfile"
+        sed -i "s/\#\#node-version\#\#/$NODE_VERSION/" $DIR_NAME/Dockerfile
+    done
 done
